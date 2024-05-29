d79a3466bb8da0a1590c3be425ceb2589aa58b78
VictorGrayDallas <VictorGrayDallas@gmail.com>
2024-05-28 19:51:54 -0500

    Refactor: Separate function to get all tasks and function for adding task

--- task.py
+++ task.py
@@ -4,28 +4,24 @@ import json
 from pathlib import Path
 import sys
 
-def save(tasks: dict, file: Path):
-	file.write_text(json.dumps(tasks))
-
-def main(args: list[str]):
-	if len(args) == 0:
-		print('Error: No arguments.')
-		return
-
-	# Ensure the file exists
+def get_file() -> Path:
 	dir = Path(Path.home(), '.simpletasks')
 	dir.mkdir(exist_ok=True)
-	file = Path(dir, 'data.json')
-	if not file.exists():
-		file.write_text('{}')
+	return Path(dir, 'data.json')
+
+def save(tasks: dict):
+	get_file().write_text(json.dumps(tasks))
+
+def get_tasks() -> dict | None:
+	file = get_file()
 	# Load and parse
 	valid = True
 	try:
-		contents = file.read_text('utf8')
+		contents = file.read_text('utf8') if file.exists else '{}'
 		tasks = json.loads(contents)
 	except FileNotFoundError:
 		print(f'Error: Could not read data file {file.absolute()}')
-		return
+		return None
 	except json.decoder.JSONDecodeError:
 		print(f'Error: Unable to parse data file {file.absolute()}')
 		valid = False
@@ -34,31 +30,46 @@ def main(args: list[str]):
 	if not valid:
 		user_input = ''
 		while len(user_input) == 0:
-			user_input = input('Delete file and make a new one? All data will be lost. y/n: ')
+			user_input = input('Delete file? All data will be lost. y/n: ')
 		if user_input[0] == 'y': # Do we even need to check for a no? For now anything other than y is considered a no.
 			file.unlink()
-			main(args)
+			tasks = {}
+		else:
+			return None
+	return tasks
+
+def add(tasks: dict, args: list[str]) -> bool:
+	if len(args) < 1:
+		print('Error: No task name given.')
+		return False
+	task_name = args[0]
+	# Ensure this task doesn't already exist.
+	if task_name in tasks:
+		print(f'Error: Task {task_name} already exists.')
+		return False
+	# Combine all remaining args for the description.
+	description = ' '.join(args[1:]) if len(args) > 1 else 'no description'
+	tasks[task_name] = { 'd': description }
+	return True
+
+def main(args: list[str]):
+	if len(args) == 0:
+		print('Error: No arguments.')
 		return
-	
+
+	tasks = get_tasks()
+	if tasks is None:
+		return
+
 	match args[0]:
 		case 'list':
 			for t in tasks:
 				print(f'{t}: {tasks[t]["d"]}')
 		case 'add':
-			if len(args) < 2:
-				print('Error: No task name given.')
-				return
-			task_name = args[1]
-			# Ensure this task doesn't already exist.
-			if task_name in tasks:
-				print(f'Error: Task {task_name} already exists.')
-				return
-			# Combine all remaining args for the description.
-			description = ' '.join(args[2::]) if len(args) > 2 else 'no description'
-			tasks[task_name] = { 'd': description }
-			save(tasks, file)
+			if add(tasks, args[1:]):
+				save(tasks)
 		case _:
 			print('default')
 
 if __name__ == '__main__':
-	main(sys.argv[1::])
+	main(sys.argv[1:])
